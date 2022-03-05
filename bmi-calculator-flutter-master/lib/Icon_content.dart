0ea6d6f463e47853c10fbe04bf374e75c0499223
joao <joao.camargo@gmail.com>
2022-03-05 13:11:54 -0300

    bmi calculator aula 131

---
+++ bmi-calculator-flutter-master/lib/Icon_content.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class IconContent extends StatelessWidget {
+  IconContent({@required this.icon, this.iconText});
+
+  final IconData icon;
+  final String iconText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          iconText,
+          style: kLabelTextStyle,
+        )
+      ],
+    );
+  }
+}
